56b8d628996c5f5af0f8ab54e59c70cc28d7443a
Richard Smith <richarddevsmith@gmail.com>
2023-04-17 10:04:19 +0200

    Initial Commit

---
+++ es/esApp.swift
@@ -0,0 +1,17 @@
+//
+//  esApp.swift
+//  es
+//
+//  Created by Richard Smith on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct esApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
